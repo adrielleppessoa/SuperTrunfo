f1afff87fbe3adec2f3e8ae9263cc803e39de547
adrielleppessoa <adriellepessoa@outlook.com.br>
2025-04-22 19:10:42 +0000

    Criei o printf

---
+++ supertrunfo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (){
+
+    char estado;
+    
+
+    // carta 1
+
+    printf("Carta 1\n");
+
+    printf("Escolha uma letra de A a H, para representar um dos oitos estados: \n");
+    scanf()
+
+
+}
